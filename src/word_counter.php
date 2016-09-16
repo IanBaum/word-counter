3d9e488701273aedd84546100c6ef623a597e887
Ian Baum <ian@ianbaum.com>
2016-09-16 08:32:30 -0400

    add logic to count words and pass first test

--- src/word_counter.php
+++ src/word_counter.php
@@ -2,7 +2,14 @@
     class word_counter{
 
         function count_word($target, $sentence){
-
+            $word_count = 0;
+            $sentence_words = explode(" ", $sentence);
+            foreach($sentence_words as $word){
+                if($word === $target){
+                    $word_count++;
+                }
+            }
+            return $word_count;
         }
     }
 ?>

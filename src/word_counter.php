0592a14a0fd30031fa8d294ae313de6fc981577e
Ian Baum <ian@ianbaum.com>
2016-09-16 08:35:15 -0400

    create and pass second test

--- src/word_counter.php
+++ src/word_counter.php
@@ -3,7 +3,8 @@
 
         function count_word($target, $sentence){
             $word_count = 0;
-            $sentence_words = explode(" ", $sentence);
+            $sentence_lower = strtolower($sentence);
+            $sentence_words = explode(" ", $sentence_lower);
             foreach($sentence_words as $word){
                 if($word === $target){
                     $word_count++;
--- tests/word_counterTest.php
+++ tests/word_counterTest.php
@@ -3,7 +3,7 @@
 
     class word_counterTest extends PHPUnit_Framework_TestCase{
 
-        function testcount_word(){
+        function testcount_word_alone(){
             //Arrange
             $testword_counter = new word_counter;
             $test_target = "dog";
@@ -14,6 +14,19 @@
 
             //Assert
             $this->assertEquals(1, $result);
+        }
+
+        function testcount_word_multi(){
+            //Arrange
+            $testword_counter = new word_counter;
+            $test_target = "dog";
+            $test_sentence = "dog dog";
+
+            //Act
+            $result = $testword_counter->count_word($test_target, $test_sentence);
+
+            //Assert
+            $this->assertEquals(2, $result);
         }
     }
 ?>
